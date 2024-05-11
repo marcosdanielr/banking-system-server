c19d43d98d55a793f5af02e43049ead817ade433
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-11 06:30:04 -0300

    chore: create bank account service should be a promisse

--- src/@core/domain/bank-account.service.ts
+++ src/@core/domain/bank-account.service.ts
@@ -4,7 +4,7 @@ import { IBankAccountRepository } from './bank-account.repository';
 export class BankAccountService {
   constructor(private bankAccountRepository: IBankAccountRepository) {}
 
-  create(account_number: string) {
+  async create(account_number: string): Promise<BankAccount> {
     const bankAccount = new BankAccount('123', 0, account_number);
 
     this.bankAccountRepository.insert(bankAccount);
