1c98adc0ab929966a38434258a723737fe7166fb
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-11 07:47:25 -0300

    refactor: remove bank accounts service (Nest module)

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -3,21 +3,16 @@ import {
   Get,
   Post,
   Body,
-  Param,
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
-import { BankAccountsService } from './bank-accounts.service';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { TransferBankAccountDto } from './dto/transfer-bank-account.dto';
 import { BankAccountService } from '../@core/domain/bank-account.service';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
-  constructor(
-    private readonly bankAccountsService: BankAccountsService,
-    private readonly bankAccountService: BankAccountService,
-  ) {}
+  constructor(private readonly bankAccountService: BankAccountService) {}
 
   @Post()
   create(@Body() createBankAccountDto: CreateBankAccountDto) {
@@ -27,11 +22,6 @@ export class BankAccountsController {
   @Get()
   findAll() {
     return this.bankAccountService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bankAccountsService.findOne(id);
   }
 
   @Post('transfer')
